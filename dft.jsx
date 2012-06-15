2f1e91bbe72f686247f8e912d2ddae12a0f39288
Hiroshi SUDA <suda@net.it-chiba.ac.jp>
2012-06-15 13:28:48 +0900

    remaked DFT

--- dft.jsx
+++ dft.jsx
@@ -1,46 +1,35 @@
 class _Main {
 	static function main( args: string[] ): void {
-		var p = 20;
-		var spectrum = new dft(p);
-		spectrum.sampling();
-		spectrum.dft();
+		var p = 10;
+		var spectrum = new dft();
+		var fn = ( x: number ):number -> {
+			return( 3.0*Math.sin(x) + 7.0*Math.cos(3.0*x) );
+		};
+		var wave = FG.sampling( fn, p );
+		spectrum.dft( wave );
 		var ar = spectrum.get_ar();
 		var ai = spectrum.get_ai();
 		for( var i=0; i<ar.length; i++ ) {
 			var x = Math.sqrt( ai[i] * ai[i] + ar[i] * ar[i] ) / p * 2.0;
-			log( x );
+			x = ((x*1000) as int)/1000;
+			log( i.toString() + ":" + x.toString() );
 		}
 	}
 }
 
 class dft {
-	var ar = [0];
-	var ai = [0];
-	var f = [0];
-	var p: number;
+	var ar = []: number[];
+	var ai = []: number[];
 
-	function constructor( p: number ) {
-		this.ar.pop();
-		this.ai.pop();
-		this.f.pop();
-		this.p = p;
-	}
-	function sampling(): void {
-		for( var i=0; i<this.p; i++ ) {
-			this.f.push( this.func_y( 2.0 * Math.PI / this.p * i) / this.p);
-		}
-	}
-	function func_y( x: number ): number {
-		return( 3.0*Math.sin(x) + 7.0*Math.cos(3.0*x) );
-	}
-	function dft(): void {
-		for( var n=0; n<this.p; n++ ) {
+	function dft( wave: number[] ): void {
+		var p = wave.length;
+		for( var n=0; n<p; n++ ) {
 			this.ar.push(0);
 			this.ai.push(0);
-			for( var m=0; m<this.p; m++ ) {
-				var x = 2.0 * Math.PI / this.p * m * n;
-				this.ar[n] += this.f[m] * Math.cos(-x);
-				this.ai[n] += this.f[m] * Math.sin(-x);
+			for( var m=0; m<p; m++ ) {
+				var x = 2.0 * Math.PI / p * m * n;
+				this.ar[n] += wave[m] * Math.cos(-x);
+				this.ai[n] += wave[m] * Math.sin(-x);
 			}
 		}
 	}
@@ -49,5 +38,15 @@ class dft {
 	}
 	function get_ai() : number[] {
 		return this.ai;
+	}
+}
+
+class FG {
+	static function sampling( f:(number)->number, num: number ): number[] {
+		var wave = []: number[];
+		for( var i=0; i<num; i++ ) {
+			wave.push( f( 2.0 * Math.PI / num * i) );
+		}
+		return wave;
 	}
 }
